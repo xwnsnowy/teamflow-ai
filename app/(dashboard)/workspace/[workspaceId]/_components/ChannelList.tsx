7242f9b4ea3b078a0fef5b87e75eb36473ad609d
Tien Thanh <tienthanhcute2k2@gmail.com>
2025-11-06 21:53:40 +0700

    feat(ChannelsList): update channel link structure and add active state styling

--- app/(dashboard)/workspace/[workspaceId]/_components/ChannelList.tsx
+++ app/(dashboard)/workspace/[workspaceId]/_components/ChannelList.tsx
@@ -3,23 +3,31 @@
 import { orpc } from '@/lib/orpc';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
 export function ChannelsList() {
   const {
     data: { channels },
   } = useSuspenseQuery(orpc.channel.list.queryOptions());
 
+  const { workspaceId, channelId } = useParams<{ workspaceId: string; channelId: string }>();
+
   return (
     <div className="space-y-1 py-1">
-      {channels.map((channel) => (
-        <Link
-          key={channel.id}
-          href={`/workspace/${channel.id}/channel/${channel.id}`}
-          className="flex justify-start items-center px-2 py-1 text-sm font-mono font-medium rounded-md border text-muted-foreground hover:text-foreground hover:bg-teal-700 transition-colors duration-200"
-        >
-          <span className="truncate">#{channel.name}</span>
-        </Link>
-      ))}
+      {channels.map((channel) => {
+        const isActive = channel.id === channelId;
+        return (
+          <Link
+            key={channel.id}
+            href={`/workspace/${workspaceId}/channel/${channel.id}`}
+            className={`flex justify-start items-center px-2 py-1 text-sm font-mono font-medium rounded-md border hover:text-foreground hover:bg-teal-700 transition-colors duration-200 ${
+              isActive ? 'text-accent-foreground bg-blue-500' : 'text-muted-foreground '
+            }`}
+          >
+            <span className="truncate">#{channel.name}</span>
+          </Link>
+        );
+      })}
     </div>
   );
 }
